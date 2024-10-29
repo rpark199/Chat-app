b4a45d77db836f47d1f1769c2e485566ae07e63d
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-10-29 19:24:43 -0400

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,30 +1,77 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View, Text, TextInput } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
- const { name, background } = route.params;
+    const { name, background } = route.params;
+    const [messages, setMessages] = useState([]);
 
- useEffect(() => {
-  navigation.setOptions({ title: name });
- }, []);
+    useEffect(() => {
+        navigation.setOptions({ title: name, color: background });
+        setMessages([
+            {
+             _id: 1,
+             text: "Hello developer",
+             createdAt: new date(),
+                user: {
+                    _id: 2,
+                    name: "React Native",
+                    avatar: "https://placeimg.com/140/140/any",
+                }
+             },
+            {
+             _id: 2,
+             text: 'This is a system message',
+             createdAt: new Date(),
+             system: true,
+            },
+        ]);
+    }, []);
 
- return (
-  <View style={[styles.container, { backgroundColor: background }]}>
-   <Text>Welcome</Text>
-  </View>
- );
+    const onSend = (newMessages) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+    }
+
+    const renderBubble = (props) => {
+        return <Bubble
+            {...props}
+            wrapperStyle={{
+                right: {
+                    backgroundColor: "#000"
+                },
+                left: {
+                    backgroundColor: "#FFF"
+                }
+            }}
+        />
+    }
+
+    return (
+        <View style={[styles.container, { backgroundColor: background }]}>
+            <GiftedChat
+                messages={messages}
+                renderBubble={renderBubble}
+                onSend={messages => onSend(messages)}
+                user={{
+                    _id: 1
+                }}
+            />
+            {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
+            {Platform.OS === 'ios' ? <KeyboardAvoidingView behavior="padding" />: null}
+        </View>
+    );
 }
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
- },
- textInput: {
-     color: "#000000",
-     backgroundColor: "#FFFFFF",
-     paddingHorizontal: 10,
-     paddingVertical: 5,
-     fontSize: 16,
-   },
+    container: {
+        flex: 1,
+    },
+    textInput: {
+        color: "#000000",
+        backgroundColor: "#FFFFFF",
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        fontSize: 16,
+    },
 });
 
 export default Chat;
