2a753b1230ae7f79cec09c86b84416992a84e097
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-10-30 21:04:19 -0400

    fetch messages from the database in real time

--- components/Chat.js
+++ components/Chat.js
@@ -1,35 +1,36 @@
 import React, { useState, useEffect } from 'react';
+import { collection, addDocs, onSnapshot, query, where, orderBy } from "firebase/firestone";
 import { StyleSheet, View, Text, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 import { Bubble, GiftedChat } from "react-native-gifted-chat";
 
-const Chat = ({ route, navigation }) => {
+const Chat = ({ route, navigation, db }) => {
     const { name, background } = route.params;
     const [messages, setMessages] = useState([]);
 
     useEffect(() => {
         navigation.setOptions({ title: name, color: background });
-        setMessages([
-            {
-             _id: 1,
-             text: "Hello developer",
-             createdAt: new Date(),
-                user: {
-                    _id: 2,
-                    name: "React Native",
-                    avatar: "https://placeimg.com/140/140/any",
-                }
-             },
-            {
-             _id: 2,
-             text: 'This is a system message',
-             createdAt: new Date(),
-             system: true,
-            },
-        ]);
     }, []);
 
+    let unsubMessages;
+
+    useEffect(() => {
+        const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+        unsubMessages = onSnapshot(q, (doc) => {
+            let newMessages = [];
+            docs.forEach(doc => {
+                newMessages.push({
+                    id: doc.id,
+                    ...doc.data(),
+                    createdAt: new Date(doc.data().createdAt.toMillis())
+                })
+            })
+            cacheMessages(newMessages);
+            setMessages(newMessages);
+        })
+    })
+
     const onSend = (newMessages) => {
-        setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+        addDoc(collection(db, "messages"), newMessages[0])
     }
 
     const renderBubble = (props) => {
@@ -53,7 +54,8 @@ const Chat = ({ route, navigation }) => {
                 renderBubble={renderBubble}
                 onSend={messages => onSend(messages)}
                 user={{
-                    _id: 1
+                    _id: userID,
+                    name: name
                 }}
             />
             {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
