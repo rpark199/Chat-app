7d1c0b0f1af14e356b8f6075b0c3363dce936129
rpark199 <147426615+rpark199@users.noreply.github.com>
2024-10-30 21:04:52 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import React from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { StyleSheet } from "react-native";
 import Start from "./components/Start";
 import Chat from "./components/Chat";
 
 const Stack = createNativeStackNavigator();
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 const App = () => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyBIxleVPX0Xea739eMwfxemldq7kvxSHFQ",
+      authDomain: "chat-app-815f1.firebaseapp.com",
+      projectId: "chat-app-815f1",
+      storageBucket: "chat-app-815f1.firebasestorage.app",
+      messagingSenderId: "1045944665896",
+      appId: "1:1045944665896:web:f87d81fb421a6f4702321d",
+      measurementId: "G-RFBP1KLV3D"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+    // Initialize Cloud Firestore and get a reference to the service
+    const db = getFirestore(app);
+
     return (
         <NavigationContainer>
             <Stack.Navigator
@@ -23,6 +42,14 @@ const App = () => {
             </Stack.Navigator>
         </NavigationContainer>
     );
+
+    const styles = StyleSheet.create({
+        container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 }
 
 export default App;
